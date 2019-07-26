b4afa40216fefbe4f6e981d849c95442c1f66fb1
bmekewar <b_mekewar@yahoo.com>
2019-07-26 21:38:09 +0530

    Basic Security

---
+++ BasicSecurity/src/main/java/com/bvm/security/HelloController.java
@@ -0,0 +1,17 @@
+package com.bvm.security;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+	@GetMapping
+	public String greeting(@RequestParam String name) {
+
+		return "Hello " + name;
+	}
+}
